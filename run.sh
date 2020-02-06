eb33027a782215cd3bb8b4c74d6c9d8cc1e638f5
Shaun Smiley <senorsmile@gmail.com>
2020-02-06 05:53:59 -0800

    a few tweaks to run.sh

--- run.sh
+++ run.sh
@@ -3,17 +3,22 @@
 set -euo pipefail
 
 source version.sh
-
 container="senorsmile-anki"
+
+mkdir -p "${HOME}/anki-docker/"
 
 opts=(
   -d
 
+  -v "${HOME}/Downloads:/root/Downloads"
+  # do NOT share with a host installed folder... create a new custom path
+  -v "${HOME}/anki2-docker:/root/.local/share/Anki2" 
   -v /etc/localtime:/etc/localtime:ro
   -v /tmp/.X11-unix:/tmp/.X11-unix # mount the X11 socket
   #-e DISPLAY=unix$DISPLAY # pass the display
-  -e "DISPLAY=${DISPLAY}"
+  -e "DISPLAY=${DISPLAY}"  # better way to pass display?
   --device /dev/snd # sound
+  --device /dev/dri # necessary?
 
   --name anki
   --restart always
